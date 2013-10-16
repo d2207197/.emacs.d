619e78a92dab311d4a5f35b65755a1435ec60570
顏孜羲 <joseph.yen@gmail.com>
2013-10-17 03:47:10 +0800

    autopair-global

--- init.el
+++ init.el
@@ -110,6 +110,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(autopair-global-mode t)
  '(custom-enabled-themes (quote (monokai)))
  '(custom-safe-themes
    (quote
