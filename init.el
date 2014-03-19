8a82b77675b2e8ba0fdd0260697caaa6572973b3
顏孜羲 <joseph.yen@gmail.com>
2014-03-19 11:36:01 +0800

    tex

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
 (package-initialize)
 (require 'dired-x)
 (require 'comment-dwim-line)
-(require 'qiang-font)
+;; (require 'qiang-font)
 (require 'tex-site)
 
 ;;;;;;;;;;;;;;
@@ -171,39 +171,262 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(exec-path
-   (quote
-    ("/usr/local/bin" "/Users/joe/.virtualenvs/linggle-flask/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin" "/usr/local/Cellar/emacs/HEAD/libexec/emacs/24.3.50/i386-apple-darwin12.4.0")))
  '(TeX-PDF-mode t)
- '(TeX-command-list (quote (("TeX" "%(PDF)%(tex) %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil (plain-tex-mode texinfo-mode ams-tex-mode) :help "Run plain TeX") ("XeLaTeX" "%`latexmk -xelatex -synctex=1%(mode)   %t" TeX-run-TeX nil t) ("LaTeX" "%`%l%(mode)%' %t" TeX-run-TeX nil (latex-mode doctex-mode) :help "Run LaTeX") ("Makeinfo" "makeinfo %t" TeX-run-compile nil (texinfo-mode) :help "Run Makeinfo with Info output") ("Makeinfo HTML" "makeinfo --html %t" TeX-run-compile nil (texinfo-mode) :help "Run Makeinfo with HTML output") ("AmSTeX" "%(PDF)amstex %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil (ams-tex-mode) :help "Run AMSTeX") ("ConTeXt" "texexec --once --texutil %(execopts)%t" TeX-run-TeX nil (context-mode) :help "Run ConTeXt once") ("ConTeXt Full" "texexec %(execopts)%t" TeX-run-TeX nil (context-mode) :help "Run ConTeXt until completion") ("BibTeX" "bibtex %s" TeX-run-BibTeX nil t :help "Run BibTeX") ("Biber" "biber %s" TeX-run-Biber nil t :help "Run Biber") ("View" "%V" TeX-run-discard-or-function t t :help "Run Viewer") ("Print" "%p" TeX-run-command t t :help "Print the file") ("Queue" "%q" TeX-run-background nil t :help "View the printer queue" :visible TeX-queue-command) ("File" "%(o?)dvips %d -o %f " TeX-run-command t t :help "Generate PostScript file") ("Index" "makeindex %s" TeX-run-command nil t :help "Create index file") ("Check" "lacheck %s" TeX-run-compile nil (latex-mode) :help "Check LaTeX file for correctness") ("Spell" "(TeX-ispell-document \"\")" TeX-run-function nil t :help "Spell-check the document") ("Clean" "TeX-clean" TeX-run-function nil t :help "Delete generated intermediate files") ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files") ("Other" "" TeX-run-command t t :help "Run an arbitrary command"))))
- '(TeX-expand-list (quote (("%p" TeX-printer-query) ("%q" (lambda nil (TeX-printer-query t))) ("%V" (lambda nil (TeX-source-correlate-start-server-maybe) (TeX-view-command-raw))) ("%vv" (lambda nil (TeX-source-correlate-start-server-maybe) (TeX-output-style-check TeX-output-view-style))) ("%v" (lambda nil (TeX-source-correlate-start-server-maybe) (TeX-style-check TeX-view-style))) ("%r" (lambda nil (TeX-style-check TeX-print-style))) ("%l" (lambda nil (TeX-style-check LaTeX-command-style))) ("%(PDF)" (lambda nil (if (and (eq TeX-engine (quote default)) (or TeX-PDF-mode TeX-DVI-via-PDFTeX)) "pdf" ""))) ("%(PDFout)" (lambda nil (cond ((and (eq TeX-engine (quote xetex)) (not TeX-PDF-mode)) " -no-pdf") ((and (eq TeX-engine (quote luatex)) (not TeX-PDF-mode)) " --output-format=dvi") ((and (eq TeX-engine (quote default)) (not TeX-PDF-mode) TeX-DVI-via-PDFTeX) " \"\\pdfoutput=0 \"") (t "")))) ("%(mode)" (lambda nil (if TeX-interactive-mode "" " -interaction=nonstopmode"))) ("%(o?)" (lambda nil (if (eq TeX-engine (quote omega)) "o" ""))) ("%(tex)" (lambda nil (eval (nth 2 (assq TeX-engine (TeX-engine-alist)))))) ("%(latex)" (lambda nil (eval (nth 3 (assq TeX-engine (TeX-engine-alist)))))) ("%(execopts)" ConTeXt-expand-options) ("%S" TeX-source-correlate-expand-options) ("%dS" TeX-source-specials-view-expand-options) ("%cS" TeX-source-specials-view-expand-client) ("%(outpage)" (lambda nil (or (when TeX-source-correlate-output-page-function (funcall TeX-source-correlate-output-page-function)) "1"))) ("%s" file nil t) ("%t" file t t) ("%`" (lambda nil (setq TeX-command-pos t TeX-command-text ""))) (" \"\\" (lambda nil (if (eq TeX-command-pos t) (setq TeX-command-pos pos pos (+ 3 pos)) (setq pos (1+ pos))))) ("\"" (lambda nil (if (numberp TeX-command-pos) (setq TeX-command-text (concat TeX-command-text (substring command TeX-command-pos (1+ pos))) command (concat (substring command 0 TeX-command-pos) (substring command (1+ pos))) pos TeX-command-pos TeX-command-pos t) (setq pos (1+ pos))))) ("%'" (lambda nil (prog1 (if (stringp TeX-command-text) (progn (setq pos (+ (length TeX-command-text) 9) TeX-command-pos (and (string-match " " (funcall file t t)) "\"")) (concat TeX-command-text " \"\\input\"")) (setq TeX-command-pos nil) "") (setq TeX-command-text nil)))) ("%n" TeX-current-line) ("%d" file "dvi" t) ("%f" file "ps" t) ("%o" (lambda nil (funcall file (TeX-output-extension) t))) ("%b" TeX-current-file-name-master-relative) ("%m" preview-create-subdirectory))))
+ '(TeX-command-list
+   (quote
+    (("TeX" "%(PDF)%(tex) %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil
+      (plain-tex-mode texinfo-mode ams-tex-mode)
+      :help "Run plain TeX")
+     ("XeLaTeX" "%`latexmk -xelatex -synctex=1%(mode)   %t" TeX-run-TeX nil t)
+     ("LaTeX" "%`%l%(mode)%' %t" TeX-run-TeX nil
+      (latex-mode doctex-mode)
+      :help "Run LaTeX")
+     ("Makeinfo" "makeinfo %t" TeX-run-compile nil
+      (texinfo-mode)
+      :help "Run Makeinfo with Info output")
+     ("Makeinfo HTML" "makeinfo --html %t" TeX-run-compile nil
+      (texinfo-mode)
+      :help "Run Makeinfo with HTML output")
+     ("AmSTeX" "%(PDF)amstex %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil
+      (ams-tex-mode)
+      :help "Run AMSTeX")
+     ("ConTeXt" "texexec --once --texutil %(execopts)%t" TeX-run-TeX nil
+      (context-mode)
+      :help "Run ConTeXt once")
+     ("ConTeXt Full" "texexec %(execopts)%t" TeX-run-TeX nil
+      (context-mode)
+      :help "Run ConTeXt until completion")
+     ("BibTeX" "bibtex %s" TeX-run-BibTeX nil t :help "Run BibTeX")
+     ("Biber" "biber %s" TeX-run-Biber nil t :help "Run Biber")
+     ("View" "%V" TeX-run-discard-or-function t t :help "Run Viewer")
+     ("Print" "%p" TeX-run-command t t :help "Print the file")
+     ("Queue" "%q" TeX-run-background nil t :help "View the printer queue" :visible TeX-queue-command)
+     ("File" "%(o?)dvips %d -o %f " TeX-run-command t t :help "Generate PostScript file")
+     ("Index" "makeindex %s" TeX-run-command nil t :help "Create index file")
+     ("Check" "lacheck %s" TeX-run-compile nil
+      (latex-mode)
+      :help "Check LaTeX file for correctness")
+     ("Spell" "(TeX-ispell-document \"\")" TeX-run-function nil t :help "Spell-check the document")
+     ("Clean" "TeX-clean" TeX-run-function nil t :help "Delete generated intermediate files")
+     ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files")
+     ("Other" "" TeX-run-command t t :help "Run an arbitrary command"))))
+ '(TeX-expand-list
+   (quote
+    (("%p" TeX-printer-query)
+     ("%q"
+      (lambda nil
+	(TeX-printer-query t)))
+     ("%V"
+      (lambda nil
+	(TeX-source-correlate-start-server-maybe)
+	(TeX-view-command-raw)))
+     ("%vv"
+      (lambda nil
+	(TeX-source-correlate-start-server-maybe)
+	(TeX-output-style-check TeX-output-view-style)))
+     ("%v"
+      (lambda nil
+	(TeX-source-correlate-start-server-maybe)
+	(TeX-style-check TeX-view-style)))
+     ("%r"
+      (lambda nil
+	(TeX-style-check TeX-print-style)))
+     ("%l"
+      (lambda nil
+	(TeX-style-check LaTeX-command-style)))
+     ("%(PDF)"
+      (lambda nil
+	(if
+	    (and
+	     (eq TeX-engine
+		 (quote default))
+	     (or TeX-PDF-mode TeX-DVI-via-PDFTeX))
+	    "pdf" "")))
+     ("%(PDFout)"
+      (lambda nil
+	(cond
+	 ((and
+	   (eq TeX-engine
+	       (quote xetex))
+	   (not TeX-PDF-mode))
+	  " -no-pdf")
+	 ((and
+	   (eq TeX-engine
+	       (quote luatex))
+	   (not TeX-PDF-mode))
+	  " --output-format=dvi")
+	 ((and
+	   (eq TeX-engine
+	       (quote default))
+	   (not TeX-PDF-mode)
+	   TeX-DVI-via-PDFTeX)
+	  " \"\\pdfoutput=0 \"")
+	 (t ""))))
+     ("%(mode)"
+      (lambda nil
+	(if TeX-interactive-mode "" " -interaction=nonstopmode")))
+     ("%(o?)"
+      (lambda nil
+	(if
+	    (eq TeX-engine
+		(quote omega))
+	    "o" "")))
+     ("%(tex)"
+      (lambda nil
+	(eval
+	 (nth 2
+	      (assq TeX-engine
+		    (TeX-engine-alist))))))
+     ("%(latex)"
+      (lambda nil
+	(eval
+	 (nth 3
+	      (assq TeX-engine
+		    (TeX-engine-alist))))))
+     ("%(execopts)" ConTeXt-expand-options)
+     ("%S" TeX-source-correlate-expand-options)
+     ("%dS" TeX-source-specials-view-expand-options)
+     ("%cS" TeX-source-specials-view-expand-client)
+     ("%(outpage)"
+      (lambda nil
+	(or
+	 (when TeX-source-correlate-output-page-function
+	   (funcall TeX-source-correlate-output-page-function))
+	 "1")))
+     ("%s" file nil t)
+     ("%t" file t t)
+     ("%`"
+      (lambda nil
+	(setq TeX-command-pos t TeX-command-text "")))
+     (" \"\\"
+      (lambda nil
+	(if
+	    (eq TeX-command-pos t)
+	    (setq TeX-command-pos pos pos
+		  (+ 3 pos))
+	  (setq pos
+		(1+ pos)))))
+     ("\""
+      (lambda nil
+	(if
+	    (numberp TeX-command-pos)
+	    (setq TeX-command-text
+		  (concat TeX-command-text
+			  (substring command TeX-command-pos
+				     (1+ pos)))
+		  command
+		  (concat
+		   (substring command 0 TeX-command-pos)
+		   (substring command
+			      (1+ pos)))
+		  pos TeX-command-pos TeX-command-pos t)
+	  (setq pos
+		(1+ pos)))))
+     ("%'"
+      (lambda nil
+	(prog1
+	    (if
+		(stringp TeX-command-text)
+		(progn
+		  (setq pos
+			(+
+			 (length TeX-command-text)
+			 9)
+			TeX-command-pos
+			(and
+			 (string-match " "
+				       (funcall file t t))
+			 "\""))
+		  (concat TeX-command-text " \"\\input\""))
+	      (setq TeX-command-pos nil)
+	      "")
+	  (setq TeX-command-text nil))))
+     ("%n" TeX-current-line)
+     ("%d" file "dvi" t)
+     ("%f" file "ps" t)
+     ("%o"
+      (lambda nil
+	(funcall file
+		 (TeX-output-extension)
+		 t)))
+     ("%b" TeX-current-file-name-master-relative)
+     ("%m" preview-create-subdirectory))))
  '(TeX-source-correlate-method (quote auto))
  '(TeX-source-correlate-mode t)
  '(TeX-source-correlate-start-server t)
- '(TeX-view-program-list (quote (("skim" "/Applications/Skim.app/Contents/SharedSupport/displayline %n %o %b") ("Preview" "open -a Preview.app %o"))))
- '(TeX-view-program-selection (quote (((output-pdf style-pstricks) "dvips and gv") (output-dvi "xdvi") (output-pdf "skim") (output-html "xdg-open"))))
+ '(TeX-view-program-list
+   (quote
+    (("skim" "/Applications/Skim.app/Contents/SharedSupport/displayline %n %o %b")
+     ("Preview" "open -a Preview.app %o"))))
+ '(TeX-view-program-selection
+   (quote
+    (((output-pdf style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "skim")
+     (output-html "xdg-open"))))
  '(ack-and-a-half-executable "ack")
  '(autopair-global-mode t)
  '(column-number-mode t)
  '(custom-enabled-themes (quote (afternoon)))
- '(custom-safe-themes (quote ("f0ea6118d1414b24c2e4babdc8e252707727e7b4ff2e791129f240a2b3093e32" "e49b9cfa39ce92b424a30cbd74386a5fcb854195cf1a8e18536388cbc2179bf6" "2283e0e235d6f00b717ccd7b1f22aa29ce042f0f845936a221012566a810773d" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "99cbc2aaa2b77374c2c06091494bd9d2ebfe6dc5f64c7ccdb36c083aff892f7d" "fa189fcf5074d4964f0a53f58d17c7e360bb8f879bd968ec4a56dc36b0013d29" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "3c708b84612872e720796ea1b069cf3c8b3e909a2e1da04131f40e307605b7f9" default)))
+ '(custom-safe-themes
+   (quote
+    ("9c26d896b2668f212f39f5b0206c5e3f0ac301611ced8a6f74afe4ee9c7e6311" "f0ea6118d1414b24c2e4babdc8e252707727e7b4ff2e791129f240a2b3093e32" "e49b9cfa39ce92b424a30cbd74386a5fcb854195cf1a8e18536388cbc2179bf6" "2283e0e235d6f00b717ccd7b1f22aa29ce042f0f845936a221012566a810773d" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "99cbc2aaa2b77374c2c06091494bd9d2ebfe6dc5f64c7ccdb36c083aff892f7d" "fa189fcf5074d4964f0a53f58d17c7e360bb8f879bd968ec4a56dc36b0013d29" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "3c708b84612872e720796ea1b069cf3c8b3e909a2e1da04131f40e307605b7f9" default)))
  '(dired-auto-revert-buffer t)
- '(dynamic-fonts-preferred-monospace-fonts (quote ("Monaco" "Consolas" "Menlo" "DejaVu Sans Mono" "Droid Sans Mono Pro" "Droid Sans Mono" "Inconsolata" "Source Code Pro" "Lucida Console" "Envy Code R" "Andale Mono" "Lucida Sans Typewriter" "monoOne" "Lucida Typewriter" "Panic Sans" "Bitstream Vera Sans Mono" "HyperFont" "PT Mono" "Ti92Pluspc" "Excalibur Monospace" "Courier New" "Courier" "Cousine" "Fira Mono" "Lekton" "Ubuntu Mono" "Liberation Mono" "BPmono" "Free Mono" "Anonymous Pro" "ProFont" "ProFontWindows" "Latin Modern Mono" "Code 2002" "ProggyCleanTT" "ProggyTinyTT" "Heiti TC")))
- '(dynamic-fonts-preferred-proportional-fonts (quote ("Lucida Grande" "Segoe UI" "DejaVu Sans" "Bitstream Vera" "Tahoma" "Verdana" "Helvetica" "Arial Unicode MS" "Arial" "Heiti TC")))
+ '(dynamic-fonts-preferred-monospace-fonts
+   (quote
+    ("Monaco" "Consolas" "Menlo" "DejaVu Sans Mono" "Droid Sans Mono Pro" "Droid Sans Mono" "Inconsolata" "Source Code Pro" "Lucida Console" "Envy Code R" "Andale Mono" "Lucida Sans Typewriter" "monoOne" "Lucida Typewriter" "Panic Sans" "Bitstream Vera Sans Mono" "HyperFont" "PT Mono" "Ti92Pluspc" "Excalibur Monospace" "Courier New" "Courier" "Cousine" "Fira Mono" "Lekton" "Ubuntu Mono" "Liberation Mono" "BPmono" "Free Mono" "Anonymous Pro" "ProFont" "ProFontWindows" "Latin Modern Mono" "Code 2002" "ProggyCleanTT" "ProggyTinyTT" "Heiti TC")))
+ '(dynamic-fonts-preferred-proportional-fonts
+   (quote
+    ("Lucida Grande" "Segoe UI" "DejaVu Sans" "Bitstream Vera" "Tahoma" "Verdana" "Helvetica" "Arial Unicode MS" "Arial" "Heiti TC")))
+ '(exec-path
+   (quote
+    ("/usr/local/bin" "/Users/joe/.virtualenvs/linggle-flask/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin" "/usr/local/Cellar/emacs/HEAD/libexec/emacs/24.3.50/i386-apple-darwin12.4.0")))
  '(global-linum-mode t)
  '(ido-everywhere nil)
  '(ido-mode (quote both) nil (ido))
  '(inhibit-startup-screen t)
  '(initial-buffer-choice "~/Coding")
- '(insert-shebang-custom-headers (quote (("py" . "#!/usr/bin/env python
-# -*- coding: utf-8 -*-") ("" . ""))))
+ '(insert-shebang-custom-headers
+   (quote
+    (("py" . "#!/usr/bin/env python
+# -*- coding: utf-8 -*-")
+     ("" . ""))))
  '(line-number-mode t)
  '(mouse-wheel-scroll-amount (quote (1 ((shift) . 1) ((control)))))
- '(org-agenda-files (quote ("~/Google Drive/Org/joe-org/notes/writeAhead/writeAhead.org")))
- '(org-export-latex-default-packages-alist (quote (("T1" "fontenc" t) ("" "fixltx2e" nil) ("" "graphicx" t) ("" "longtable" nil) ("" "float" nil) ("" "wrapfig" nil) ("" "soul" t) ("" "textcomp" t) ("" "marvosym" t) ("" "wasysym" t) ("" "latexsym" t) ("" "amssymb" t) ("" "hyperref" nil) "\\tolerance=1000")))
- '(org-latex-to-pdf-process (quote ("xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f")))
- '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/") ("marmalade" . "http://marmalade-repo.org/packages/") ("org" . "http://orgmode.org/elpa/"))))
- '(preview-LaTeX-command (quote ("%`%l \"\\nonstopmode\\nofiles\\PassOptionsToPackage{" ("," . preview-required-option-list) "}{preview}\\AtBeginDocument{\\ifx\\ifPreview\\undefined" preview-default-preamble "\\fi}\"%' %t")))
+ '(org-agenda-files
+   (quote
+    ("~/Google Drive/Org/joe-org/notes/writeAhead/writeAhead.org")))
+ '(org-export-latex-default-packages-alist
+   (quote
+    (("T1" "fontenc" t)
+     ("" "fixltx2e" nil)
+     ("" "graphicx" t)
+     ("" "longtable" nil)
+     ("" "float" nil)
+     ("" "wrapfig" nil)
+     ("" "soul" t)
+     ("" "textcomp" t)
+     ("" "marvosym" t)
+     ("" "wasysym" t)
+     ("" "latexsym" t)
+     ("" "amssymb" t)
+     ("" "hyperref" nil)
+     "\\tolerance=1000")))
+ '(org-latex-to-pdf-process
+   (quote
+    ("xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f")))
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "http://melpa.milkbox.net/packages/")
+     ("marmalade" . "http://marmalade-repo.org/packages/")
+     ("org" . "http://orgmode.org/elpa/"))))
+ '(preview-LaTeX-command
+   (quote
+    ("%`%l \"\\nonstopmode\\nofiles\\PassOptionsToPackage{"
+     ("," . preview-required-option-list)
+     "}{preview}\\AtBeginDocument{\\ifx\\ifPreview\\undefined" preview-default-preamble "\\fi}\"%' %t")))
  '(recentf-mode t)
  '(safe-local-variable-values (quote ((TeX-engine . XeLaTeX))))
  '(scroll-margin 5)
