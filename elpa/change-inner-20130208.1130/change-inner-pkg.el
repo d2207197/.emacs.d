c43b55d741adf13d35321b37be1f689ee62fe569
顏孜羲 <joseph.yen@gmail.com>
2015-02-28 14:04:32 +0800

    upgrade/install packages

---
+++ elpa/change-inner-20130208.1130/change-inner-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "change-inner" "20130208.1130" "Change contents based on semantic units" '((expand-region "0.7")) :keywords '("convenience" "extensions"))
---
+++ elpa/loc-changes-20150218.400/loc-changes-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "loc-changes" "20150218.400" "keep track of positions even after buffer changes" 'nil :url "http://github.com/rocky/emacs-loc-changes")
